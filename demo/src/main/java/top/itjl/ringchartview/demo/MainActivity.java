cad2e74b1ee48f6ef1b09c726c07c3c0e87e76c4
wb_wangjinglong03 <wb_wangjinglong@meituan.com>
2025-03-10 16:19:07 +0800

    使用AI IDE自动优化

--- demo/src/main/java/top/itjl/ringchartview/demo/MainActivity.java
+++ demo/src/main/java/top/itjl/ringchartview/demo/MainActivity.java
@@ -1,9 +1,7 @@
 package top.itjl.ringchartview.demo;
 
-import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,26 +9,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 import top.itjl.ringchartview.RingChartView;
+import top.itjl.ringchartview.demo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    private RingChartView ringChartView1,ringChartView2,ringChartView3;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setupRingChartView2();
+    }
 
-        ringChartView1=findViewById(R.id.ring_chart_view1);
-        ringChartView2=findViewById(R.id.ring_chart_view2);
-        ringChartView3=findViewById(R.id.ring_chart_view3);
-
+    private void setupRingChartView2() {
+        RingChartView ringChartView2 = binding.ringChartView2;
         ringChartView2.setMultiProgress(true);
-        List<RingChartView.ProgressNode> nodeList=new ArrayList<>();
-        nodeList.add(new RingChartView.ProgressNode(10, Color.GREEN));
-        nodeList.add(new RingChartView.ProgressNode(20, Color.BLUE));
-        nodeList.add(new RingChartView.ProgressNode(50, Color.RED));
-        nodeList.add(new RingChartView.ProgressNode(10, Color.YELLOW));
+
+        List<RingChartView.ProgressNode> nodeList = new ArrayList<>();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            nodeList.add(new RingChartView.ProgressNode(10, getColor(R.color.chart_green)));
+            nodeList.add(new RingChartView.ProgressNode(20, getColor(R.color.chart_blue)));
+            nodeList.add(new RingChartView.ProgressNode(50, getColor(R.color.chart_red)));
+            nodeList.add(new RingChartView.ProgressNode(10, getColor(R.color.chart_yellow)));
+        }
         ringChartView2.setProgressNodes(nodeList);
         ringChartView2.setMaxValue(100);
     }
@@ -39,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-//        ringChartView1.playAnimation();
+        binding.ringChartView1.playAnimation();
+        binding.ringChartView2.playAnimation();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        binding.ringChartView1.stopAnimation();
+        binding.ringChartView2.stopAnimation();
     }
 }
